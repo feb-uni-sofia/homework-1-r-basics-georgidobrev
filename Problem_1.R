2d8a0ac1e76e42daf91206628ec6959cfb035975
Georgi Dobrev <georgidobrev.5@gmail.com>
2018-03-31 12:28:00 +0300

    Update

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,28 @@
+#Problem_1_Georgi Dobrev - 700890
+
+# a)
+x <- c(4, 1, 1, 4)
+
+# b)
+y <- c(1, 4)
+
+# c)
+x-y
+# 3 -3  0  0
+
+# d)
+s <- c(x,y)
+
+# e)
+rep (s, 10)
+
+# f)
+rep(s, each = 3)
+
+# g)
+seq(7, 21, by=1)
+
+# h)
+length(seq(7, 21, by=1)
+       )
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,47 @@
+#Problem_2_Georgi Dobrev - 700890
 
+# a)
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+# b)
+xmax - xmin
+# 2.0 2.3 3.0 7.0 6.0 3.0 5.9
+
+# c)
+mean (xmax)
+# 25.57143
+
+mean (xmin)
+# 21.4
+
+# d)
+xmin [xmin<21.4]
+
+# e)
+xmin [xmax>25.57143]
+
+xmax [xmax>25.57143]
+
+# Use the subsetting operator [] to select those elements of xmin where
+# the maximal temperature was above its weekly average. 
+# Note: I think that xmin was supposed to be xmax. But I'm including both of them. 
+
+# f)
+
+"Minimum temperatures" <- xmin
+"Maximum temperatures" <- xmax
+
+# g)
+temperatures <- data.frame(xmin,xmax)
+
+# h)
+temperatures["xminFahrenheit"] <- temperatures$xmin*9/5+32
+# I'm not sure if I had to create the new column in the temperatures dataframe, 
+# but the principle is the same.
+
+# i)
+fahrenheit <- data.frame(temperatures$xminFahrenheit)
+
+# j)
+fahrenheitFiveDays <- data.frame(fahrenheit$temperatures.xminFahrenheit[1:5])
